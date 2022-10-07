2c4e605c6fcb0d0d3c49bca0b1e0129909f11d43
enyaaad <enindima1@gmail.com>
2022-10-07 17:21:16 +0700

    init proj and added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlanetsComponent } from './planets/planets.component';
+import { ResidentsComponent } from './residents/residents.component';
+import {RouterModule, Routes} from "@angular/router";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const appRoutes: Routes =[
+  {path:'planets',component: PlanetsComponent},
+  {path:'residents',component: ResidentsComponent},
+  {path: '',   redirectTo: '/planets', pathMatch: 'full' },
+  {path: '**', component: PageNotFoundComponent }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetsComponent,
+    ResidentsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes,{enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/residents/residents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-residents',
+  templateUrl: './residents.component.html',
+  styleUrls: ['./residents.component.sass']
+})
+export class ResidentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
